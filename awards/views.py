d4e00678ae62b9b07dad941d644ce8ac4c0525e1
Robin <robinkaris7@gmail.com>
2019-10-19 07:33:02 +0300

    this commit adds a home view

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+def home(request):
+    all_projects = Project.fetch_all_images()
+    return render(request,"/index.html")
