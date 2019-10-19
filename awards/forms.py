e86ce39e61bb02d924b64f16db32506e2335d7a9
Robin <robinkaris7@gmail.com>
2019-10-19 09:42:54 +0300

    this commit adds voteform

--- awards/forms.py
+++ awards/forms.py
@@ -5,3 +5,10 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         exclude = ['user','user_project_id']
+
+
+
+class VoteForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ('design','usability','content')
--- awards/views.py
+++ awards/views.py
@@ -1,6 +1,40 @@
 from django.shortcuts import render,redirect
-
+from .models import Project
 # Create your views here.
 def home(request):
-    
-    return render(request,"index.html")
+    all_projects = Project.fetch_all_images()
+    return render(request,"index.html",{"all_images":all_projects})
+
+
+
+
+
+def project_review(request,project_id):
+    try:
+        single_project = Project.get_single_project(project_id)
+        average_score = round(((single_project.design + single_project.usability + single_project.content)/3),2)
+        if request.method == 'POST':
+            vote_form = VoteForm(request.POST)
+            if vote_form.is_valid():
+                single_project.vote_submissions+=1
+                if single_project.design == 0:
+                    single_project.design = int(request.POST['design'])
+                else:
+                    single_project.design = (single_project.design + int(request.POST['design']))/2
+                if single_project.usability == 0:
+                    single_project.usability = int(request.POST['usability'])
+                else:
+                    single_project.usability = (single_project.usability + int(request.POST['usability']))/2
+                if single_project.content == 0:
+                    single_project.content = int(request.POST['content'])
+                else:
+                    single_project.content = (single_project.content + int(request.POST['usability']))/2
+
+                single_project.save()
+                return redirect('project_review',project_id)
+        else:
+            vote_form = VoteForm()
+
+    except Exception as  e:
+        raise Http404()
+    return render(request,'Moringa_Project_Awards/project_review.html',{"vote_form":vote_form,"single_project":single_project,"average_score":average_score})    
