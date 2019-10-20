dde184d8715e7ed3a84b0d9810bdfb8583e9ad75
Robin <robinkaris7@gmail.com>
2019-10-20 09:54:22 +0300

    this commit adds project serializer class

--- awards/serializer.py
+++ awards/serializer.py
@@ -5,3 +5,9 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ('profile_picture','prof_user','bio','all_projects')
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('title','details','link','image','design','usability','content')
