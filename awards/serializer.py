85efd994144421d02006acdae95883938d29f759
Robin <robinkaris7@gmail.com>
2019-10-20 09:53:30 +0300

    this commit creates profile serializer class

--- awards/serializer.py
+++ awards/serializer.py
@@ -1,3 +1,7 @@
 from rest_framework import serializers
 from .models import Project,Profile
 
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('profile_picture','prof_user','bio','all_projects')
